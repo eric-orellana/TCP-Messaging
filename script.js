0ccb8aae5b130f2dff90ef8aeeefbe3715a6a6ed
Eric Orellana <xinine4321i@yahoo.com>
2024-07-19 22:31:58 -0400

    s

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     const messageElement = document.getElementById('message');
-    messageElement.textContent = "Hello Katie!";
+    messageElement.textContent = "Hello Bruh";
 });
